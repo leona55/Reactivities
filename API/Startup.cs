2bf72af0fd51aca76ab806e5631724cc3aad9ffe
leona55 <mavrajn@gmail.com>
2021-08-20 12:26:39 -0200

    commit

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,7 +32,11 @@ namespace API
             {
                  opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
              });
-
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy",policy => {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
             
         }
 
@@ -49,6 +53,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
